637b32f074519231befd99c92127a22568a314bc
Akino-K <akino_1107@yahoo.ne.jp>
2026-09-10 14:04:50 +0900

    BaseDataObj.Mgr root追加処理 取り消し

--- NxRebuild.shared/BaseDataObjMgr.cs
+++ NxRebuild.shared/BaseDataObjMgr.cs
@@ -119,36 +119,6 @@ namespace NxRebuild.shared {
         }
         protected abstract TKey GenerateDataID();
 
-        public virtual T CreateRoot() {
-            var root = new T();
-
-            root.DBcon = DBcon;
-            root.SelfObjMgr = this;
-
-            // ルートは物理レコードを持たないので空スキーマでよい
-            root.Setproperties(new Dictionary<string, object?>());
-
-            root.TenantCode = TenantCode;
-            root.CurrUsrID = CurrentUserID;
-
-            // ★ GenerateDataID を使わず、型に応じて 0 / Guid.Empty をセット
-            if (typeof(TKey) == typeof(int))
-                root.DataID = (TKey)(object)0;
-            else if (typeof(TKey) == typeof(Guid))
-                root.DataID = (TKey)(object)Guid.Empty;
-            else
-                throw new NotSupportedException("Unsupported TKey type for CreateRoot.");
-
-            root.DataName = RootName;
-
-            root.DataType = NxDataType.root;
-
-            // ルートは親を持たない
-            root.ParentDataObj = null;
-
-            _dataList.Add(root);
-            return root;
-        }
 
         public virtual T CreateNewDataObj() {
             var dataObj = new T();
